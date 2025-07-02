b5b0202d7ffac90c915380e66163f4a328adc54f
Pedro Vitor da Silva Melo Leonez <147958375+pedroleonez@users.noreply.github.com>
2025-07-02 03:48:46 +0000

    feat: create and impl Agendamento.h

---
+++ Agendamento.h
@@ -0,0 +1,10 @@
+#ifndef AGENDAMENTO_H
+#define AGENDAMENTO_H
+
+#include <vector>
+#include "Corretor.h"
+#include "Imovel.h"
+
+void gerarAgendamento(std::vector<Corretor>& corretores, std::vector<Imovel>& imoveis);
+
+#endif
